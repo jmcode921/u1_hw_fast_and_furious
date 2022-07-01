733f4dc6bc352f20affcc9cd2c54624557bc1713
Jesus Morales <jesusmorales7499@gmail.com>
2022-06-30 22:10:38 -0400

    finish hw

--- script.js
+++ script.js
@@ -20,16 +20,35 @@ window.onload =  function(event) {
   // Do all of your work inside the window.onload function (in other words, here!)
 
   // Part 1
-
+document.getElementById("main-title").innerHTML = "Doms Page"
   // Part 2
-
+document.body.style.backgroundColor = "blue";
   // Part 3
-
+document.querySelectorAll('li')[5].remove()
   // Part 4
-
+let newFont =document.querySelectorAll('.special-title')
+  newFont.forEach((newFont)=>{
+    newFont.style.fontSize = '2em';
+  }) 
   // Part 5
-
+document.querySelector('#past-races').querySelectorAll('li')[3].remove()
   // Part 6
-
+const newList =document.createElement('li')
+newList.appendChild(document.createTextNode('Atlanta'))
+document.querySelector('#past-races').appendChild(newList)
   // Part 7
+  const myCity =document.querySelector('.main')
+
+  const blogPost =document.createElement('div')
+  blogPost.setAttribute('class','blog-post purple')
+
+  const h1 =document.createElement('h1')
+  h1.innerText = 'Atlanta'
+  blogPost.append(h1)
+
+  myCity.appendChild(blogPost)
+
+  const newP = document.createElement('p')
+  newP.innerText = 'Atlanta is the best city in the USA!'
+  blogPost.appendChild(newP)
 }
